3800cd09f462233acb23334c2db16ae75fb60a38
clayton craft <craftyguy@users.noreply.github.com>
2017-08-15 11:10:04 -0700

    Do not modprobe loop if /dev/loop* exists (#391)

--- pmb/install/losetup.py
+++ pmb/install/losetup.py
@@ -27,7 +27,8 @@ import pmb.chroot
 
 
 def init(args):
-    pmb.helpers.run.root(args, ["modprobe", "loop"])
+    if not glob.glob("/dev/loop*"):
+        pmb.helpers.run.root(args, ["modprobe", "loop"])
     loopdevices = [loopdev for loopdev in glob.glob("/dev/loop*") if not os.path.isdir(loopdev)]
     for loopdev in loopdevices:
         pmb.helpers.mount.bind_blockdevice(args, loopdev,
