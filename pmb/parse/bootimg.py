42018f80146e9c1049327e35ecbbbaf0b2eedf19
Lucas Ramage <ramage.lucas@openmailbox.org>
2018-02-11 06:41:41 -0500

    mkbootimg: replace Python version with osm0sis' C version (#1193)
    Closes #441. Adjust bootimg_analyze code:
    * Install mkbootimg (which now provides unpackbootimg) instead of
      unpackbootimg. In theory, pmbootstrap should recognize this
      automatically, however right now it does not yet handle this case.
    * The file names of the extracted files have changed.

--- pmb/parse/bootimg.py
+++ pmb/parse/bootimg.py
@@ -27,7 +27,7 @@ def bootimg(args, path):
 
     logging.info("NOTE: You will be prompted for your sudo password, so we can set"
                  " up a chroot to extract and analyze your boot.img file")
-    pmb.chroot.apk.install(args, ["file", "unpackbootimg"])
+    pmb.chroot.apk.install(args, ["file", "mkbootimg"])
 
     temp_path = pmb.chroot.other.tempfolder(args, "/tmp/bootimg_parser")
     bootimg_path = args.work + "/chroot_native" + temp_path + "/boot.img"
@@ -52,20 +52,20 @@ def bootimg(args, path):
     # Get base, offsets, pagesize, cmdline and qcdt info
     with open(bootimg_path + "-base", 'r') as f:
         output["base"] = ("0x%08x" % int(f.read().replace('\n', ''), 16))
-    with open(bootimg_path + "-kernel_offset", 'r') as f:
+    with open(bootimg_path + "-kerneloff", 'r') as f:
         output["kernel_offset"] = ("0x%08x" % int(f.read().replace('\n', ''), 16))
-    with open(bootimg_path + "-ramdisk_offset", 'r') as f:
+    with open(bootimg_path + "-ramdiskoff", 'r') as f:
         output["ramdisk_offset"] = ("0x%08x" % int(f.read().replace('\n', ''), 16))
-    with open(bootimg_path + "-second_offset", 'r') as f:
+    with open(bootimg_path + "-secondoff", 'r') as f:
         output["second_offset"] = ("0x%08x" % int(f.read().replace('\n', ''), 16))
-    with open(bootimg_path + "-tags_offset", 'r') as f:
+    with open(bootimg_path + "-tagsoff", 'r') as f:
         output["tags_offset"] = ("0x%08x" % int(f.read().replace('\n', ''), 16))
     with open(bootimg_path + "-pagesize", 'r') as f:
         output["pagesize"] = f.read().replace('\n', '')
     with open(bootimg_path + "-cmdline", 'r') as f:
         output["cmdline"] = f.read().replace('\n', '')
-    output["qcdt"] = ("true" if os.path.isfile(bootimg_path + "-dt") and
-                      os.path.getsize(bootimg_path + "-dt") > 0 else "false")
+    output["qcdt"] = ("true" if os.path.isfile(bootimg_path + "-dtb") and
+                      os.path.getsize(bootimg_path + "-dtb") > 0 else "false")
 
     # Cleanup
     pmb.chroot.root(args, ["rm", "-r", temp_path])
