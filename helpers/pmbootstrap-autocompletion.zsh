f45c87eadc06cc44137070a9b22f22c57c8dfd40
Daniil Yarancev <21169548+Yardanico@users.noreply.github.com>
2018-06-14 23:19:58 +0300

    zsh autocompletion: fix filename in comment (#1564)

--- helpers/pmbootstrap-autocompletion.zsh
+++ helpers/pmbootstrap-autocompletion.zsh
@@ -5,7 +5,7 @@
 # somewhere that makes sense to you). Then, insert the following line
 # in your ~/.zshrc (making sure to use the right folder name, if changed):
 #
-#	source ~/.zsh/pmbootstrap-auto-completion.zsh
+#	source ~/.zsh/pmbootstrap-autocompletion.zsh
 #
 # Then, set the variable PMBOOTSTRAP_DIR to your `pmbootstrap` root.
 # Example:
