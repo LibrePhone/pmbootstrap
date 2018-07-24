LibrePhone/pmbootstrap

commit 8268dc0e3d365772dadab890af31e35f5129bd28

pmbootstrap: kill process if silent for 5 minutes (rewrite logging)

pmb/build/menuconfig.py
@@ -77,7 +77,7 @@ def get_outputdir(args, pkgname):
     cmd = "srcdir=/home/pmos/build/src source APKBUILD; echo $builddir"
     ret = pmb.chroot.user(args, ["sh", "-c", cmd],
                           "native", "/home/pmos/build",
-                          return_stdout=True).rstrip()
+                          output_return=True).rstrip()
     if os.path.exists(chroot + ret + "/.config"):
         return ret
 
@@ -129,13 +129,14 @@ def menuconfig(args, pkgname):
     pmb.chroot.user(args, ["abuild", "unpack"], "native", "/home/pmos/build")
     logging.info("(native) apply patches")
     pmb.chroot.user(args, ["abuild", "prepare"], "native",
-                    "/home/pmos/build", log=False, env={"CARCH": arch})
+                    "/home/pmos/build", output="interactive",
+                    env={"CARCH": arch})
 
     # Run make menuconfig
     outputdir = get_outputdir(args, pkgname)
     logging.info("(native) make " + kopt)
     pmb.chroot.user(args, ["make", kopt], "native",
-                    outputdir, log=False,
+                    outputdir, output="interactive",
                     env={"ARCH": pmb.parse.arch.alpine_to_kernel(arch),
                          "DISPLAY": os.environ.get("DISPLAY"),
                          "XAUTHORITY": "/home/pmos/.Xauthority"})

.gitlab/setup-pmos-environment.sh
@@ -5,11 +5,18 @@
 # all of these configurations to save time, at least for now.
 # Author: Clayton Craft <clayton@craftyguy.net>
 
+# skip non-shared runner
 [[ -d "/home/pmos" ]] && echo "pmos user already exists, assume running on pre-configured runner" && exit
+
+# mount binfmt_misc
 mount -t binfmt_misc none /proc/sys/fs/binfmt_misc
+
+# install dependencies (procps: /bin/kill)
 apt update
-apt install -q -y git sudo shellcheck
+apt install -q -y git sudo shellcheck procps
 pip3 install virtualenv
+
+# create pmos user
 echo "Creating pmos user"
 useradd pmos -m -s /bin/bash -b "/home"
 chown -R pmos:pmos .

pmb/__init__.py
@@ -71,15 +71,16 @@ def main():
 
     except Exception as e:
         logging.info("ERROR: " + str(e))
-        if os.path.exists(args.log):
-            # Hint to read the log file (only gets printed to stdout)
-            print("Run 'pmbootstrap log' for details.")
-        else:
-            logging.info("Crashed before the log file was created.")
-            logging.info("Running init again like the following gives more details:")
-            logging.info("    pmbootstrap --details-to-stdout init")
         logging.info("See also: <https://postmarketos.org/troubleshooting>")
         logging.debug(traceback.format_exc())
+
+        # Hints about the log file (print to stdout only)
+        if os.path.exists(args.log):
+            print("Run 'pmbootstrap log' for details.")
+        else:
+            print("Crashed before the log file was created.")
+            print("Running init again like the following gives more details:")
+            print("    pmbootstrap --details-to-stdout init")
         return 1
 
 

pmb/aportgen/busybox_static.py
@@ -54,8 +54,8 @@ def generate(args, pkgname):
     # Hash the distfile
     hashes = pmb.chroot.user(args, ["sha512sum",
                                     "busybox-static-" + version + "-" + arch + ".apk"],
-                             "buildroot_" + arch, working_dir="/var/cache/distfiles",
-                             return_stdout=True)
+                             "buildroot_" + arch, "/var/cache/distfiles",
+                             output_return=True)
 
     # Write the APKBUILD
     pmb.helpers.run.user(args, ["mkdir", "-p", args.work + "/aportgen"])

pmb/aportgen/musl.py
@@ -60,7 +60,7 @@ def generate(args, pkgname):
     hashes = pmb.chroot.user(args, ["sha512sum",
                                     "musl-" + version + "-" + arch + ".apk",
                                     "musl-dev-" + version + "-" + arch + ".apk"], "buildroot_" + arch,
-                             working_dir="/var/cache/distfiles", return_stdout=True)
+                             "/var/cache/distfiles", output_return=True)
 
     # Write the APKBUILD
     pmb.helpers.run.user(args, ["mkdir", "-p", args.work + "/aportgen"])

pmb/build/other.py
@@ -84,13 +84,12 @@ def copy_to_buildpath(args, package, suffix="native"):
     # Clean up folder
     build = args.work + "/chroot_" + suffix + "/home/pmos/build"
     if os.path.exists(build):
-        pmb.chroot.root(args, ["rm", "-rf", "/home/pmos/build"],
-                        suffix=suffix)
+        pmb.chroot.root(args, ["rm", "-rf", "/home/pmos/build"], suffix)
 
     # Copy aport contents
     pmb.helpers.run.root(args, ["cp", "-r", aport + "/", build])
     pmb.chroot.root(args, ["chown", "-R", "pmos:pmos",
-                           "/home/pmos/build"], suffix=suffix)
+                           "/home/pmos/build"], suffix)
 
 
 def is_necessary(args, arch, apkbuild, indexes=None):

pmb/chroot/apk.py
@@ -52,8 +52,7 @@ def update_repository_list(args, suffix="native", check=False):
             for line in handle:
                 lines_old.append(line[:-1])
     else:
-        pmb.helpers.run.root(args, ["mkdir", "-p", os.path.dirname(path)],
-                             suffix)
+        pmb.helpers.run.root(args, ["mkdir", "-p", os.path.dirname(path)])
 
     # Up to date: Save cache, return
     lines_new = pmb.helpers.repo.urls(args)

pmb/chroot/apk_static.py
@@ -102,7 +102,7 @@ def verify_signature(args, files, sigkey_path):
         pmb.helpers.run.user(args, ["openssl", "dgst", "-sha1", "-verify",
                                     sigkey_path, "-signature", files[
                                         "sig"]["temp_path"],
-                                    files["apk"]["temp_path"]], check=True)
+                                    files["apk"]["temp_path"]])
     except BaseException:
         os.unlink(files["sig"]["temp_path"])
         os.unlink(files["apk"]["temp_path"])
@@ -133,7 +133,7 @@ def extract(args, version, apk_path):
                   " (must match the package version " + version + ")")
     os.chmod(temp_path, os.stat(temp_path).st_mode | stat.S_IEXEC)
     version_bin = pmb.helpers.run.user(args, [temp_path, "--version"],
-                                       check=True, return_stdout=True)
+                                       output_return=True)
     version_bin = version_bin.split(" ")[1].split(",")[0]
     if not version.startswith(version_bin + "-r"):
         os.unlink(temp_path)
@@ -179,6 +179,5 @@ def init(args):
     extract(args, version, apk_static)
 
 
-def run(args, parameters, check=True):
-    pmb.helpers.run.root(
-        args, [args.work + "/apk.static"] + parameters, check=check)
+def run(args, parameters):
+    pmb.helpers.run.root(args, [args.work + "/apk.static"] + parameters)

commit ed59e38e62aa6936fadadb6b5495f51b9f068704

pmb: fix kconfig edit
Using the default UI (menuconfig) was broken since 8268dc0e, where
logging was rewritten. The wrong output mode has been used, "tui" is
the right one.

pmb/build/menuconfig.py
@@ -136,7 +136,7 @@ def menuconfig(args, pkgname):
     outputdir = get_outputdir(args, pkgname)
     logging.info("(native) make " + kopt)
     pmb.chroot.user(args, ["make", kopt], "native",
-                    outputdir, output="interactive",
+                    outputdir, output="tui",
                     env={"ARCH": pmb.parse.arch.alpine_to_kernel(arch),
                          "DISPLAY": os.environ.get("DISPLAY"),
                          "XAUTHORITY": "/home/pmos/.Xauthority"})